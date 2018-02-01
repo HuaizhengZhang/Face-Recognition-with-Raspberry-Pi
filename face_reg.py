e82d45010b19b8486a4e4b1ca9ce025fcc80640c
HuaizhengZhang <huaizhen001@e.ntu.edu.sg>
2018-02-01 21:37:57 +0800

    test mobilenet

--- face_reg.py
+++ face_reg.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 from keras.preprocessing.image import img_to_array
 from keras.models import model_from_json
+from picamera.arry import PiRGBArray
+from picamera import PiCamera
 import time
 
 with open('face_model_3.json', 'rb') as json_file:
@@ -12,16 +14,15 @@ model.load_weights('face_model_3.h5')
 print "Load model successfully"
 
 
-print "Start video stream"
 camera = PiCamera()
-camera.resolution = (320,240)
+camera.resolution = (600,600)
 camera.framerate = 32
-cap = PiRGBArray(camera, size=(320,240))
+cap = PiRGBArray(camera, size=(600, 600))
+print "Start video stream"
 
 THD = 0.7
 for i in camera.capture_continuous(cap, format='bgr', use_video_port=True):
     frame = i.array
-    frame = imutils.resize(frame, width=400)
     image = cv2.resize(frame, (150, 150))
     image = image.astype('float') / 255.0
     image = img_to_array(image)
@@ -30,17 +31,17 @@ for i in camera.capture_continuous(cap, format='bgr', use_video_port=True):
     predict = model.predict(image)[0]
     
     if predict[0] > THD:
-        frame = cv2.putText(frame, "Hello, Dr.Luo Yong", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, "Hello, Dr.Luo Yong", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     elif predict[1] > THD:
-        frame = cv2.putText(frame, "Hello, Wang Yongjie", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, "Hello, Wang Yongjie", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     elif predict[2] > THD:
-        frame = cv2.putText(frame, "Hello, Yi Deliang", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, "Hello, Yi Deliang", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     elif predict[3] > THD:
-        frame = cv2.putText(frame, "Hello, Zhang Huaizheng", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, "Hello, Zhang Huaizheng", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     elif predict[4] > THD:
-        frame = cv2.putText(frame, "Hello, Dr.Zhou Xin", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, "Hello, Dr.Zhou Xin", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     else:
-        frame = cv2.putText(frame, "Can not recognize", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, "Can not recognize", (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
---
+++ mobilenet_pi.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+from keras.preprocessing import image
+from keras.models import load_model
+from keras.applications.mobilenet import MobileNet, preprocess_input, decode_predictions
+import time
+
+model = MobileNet(weights='imagenet')
+print "Load model successfully"
+
+
+camera = PiCamera()
+camera.resolution = (600,600)
+camera.framerate = 32
+cap = PiRGBArray(camera, size=(600, 600))
+print "Start video stream"
+
+
+for i in camera.capture_continuous(cap, format='bgr', use_video_port=True):
+    frame = i.array
+   
+    img = image.load_img(frame, target_size=(224, 224))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    
+    preds = model.predict(x)
+    
+    
+    cv2.putText(frame, 'Predicted: ' + str(decode_predictions(preds, top=1)[0]), (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    
+    
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    cap.truncate(0)
+    if key == ord('q'):
+        break
